46cb6280e873d090e18895512b073ee87823a527
msofi <msofiarached@gmail.com>
2023-03-14 15:23:51 -0300

    Listos los cambios

--- TP0/src/Main/Main.java
+++ TP0/src/Main/Main.java
@@ -1,10 +1,42 @@
 package Main;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import Modelo.TiempoAnemico;
+import Modelo.TiempoNoAnemico;
+import Modelo.TiempoRecord;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+				System.out.println("****Anemica****");
+				TiempoAnemico miTiempoAnemico = new TiempoAnemico(LocalDate.now());
 
+				System.out.println(DateTimeFormatter.ofPattern("dd-MM-yyyy").format(miTiempoAnemico.getMyDate()));
+
+				System.out.println(
+				DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy ").format(miTiempoAnemico.getMyDate()) + "\n");
+
+				// De manera no anemica, pidiendo directamente el formato que quiero ya que lo
+				// tiene como un comportamiento //
+				System.out.println("\n****No Anemica****");
+
+				TiempoNoAnemico miTiempoNoAnemico = new TiempoNoAnemico();
+
+				miTiempoNoAnemico.verFechaCorta();
+				miTiempoNoAnemico.verFechaCompleta();
+
+		
+				System.out.println("\n****Records ****");
+
+				TiempoRecord miTiempoRecord = new TiempoRecord(LocalDate.now());
+
+				System.out.println(DateTimeFormatter.ofPattern("dd-MM-yyyy").format(miTiempoRecord.fecha()));
+
+				System.out.println(DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy ").format(miTiempoRecord.fecha()));
+
+			}
 	}
 
-}
--- TP0/src/Modelo/TiempoNoAnemico.java
+++ TP0/src/Modelo/TiempoNoAnemico.java
@@ -1,5 +1,21 @@
 package Modelo;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class TiempoNoAnemico {
+	private LocalDate fecha;
+
+	public TiempoNoAnemico() {
+		this.fecha = LocalDate.now();
+	}
+
+	public void verFechaCorta() {
+		System.out.println(DateTimeFormatter.ofPattern("dd-MM-yyyy").format(fecha));
+	}
+
+	public void verFechaCompleta() {
+		System.out.println(DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy ").format(fecha) + "\n");
+	}
 
 }
